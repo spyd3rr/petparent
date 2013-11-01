368c36ae641906fc5e91e04cab59a40080f455af
venky-D <venkat@startupsourcing.com>
2013-11-01 17:49:52 +0530

    category field

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,11 +1,8 @@
 class Tag < ParseResource::Base
-  fields :name
+  fields :name,:category
   #belongs_to :venue
   #belongs_to :event
 
-  def to_pointer
-    klass_name = self.class.model_name.to_s
-    {"__type" => "Pointer", "className" => klass_name.to_s, "objectId" => self.id}
-  end
+
 
 end
